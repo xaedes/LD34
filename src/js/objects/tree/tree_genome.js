3982eb4a6b932c0db0e07c1cc38d666c1374baa4
xaedes <xaedes@gmail.com>
2015-12-14 01:59:21 +0100

    added (empty) TreeGenome class to hold parameters

---
+++ src/js/objects/tree/tree_genome.js
@@ -0,0 +1,9 @@
+'use strict';
+
+define(['phaser'], function(Phaser) {
+    function TreeGenome() {
+        // parameter class to hold all parameters relevant to generate a tree
+    }
+    TreeGenome.prototype.constructor = TreeGenome;
+    return TreeGenome;
+});
